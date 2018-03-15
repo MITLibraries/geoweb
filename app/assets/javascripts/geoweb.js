7e0d865ad51b8bd7ef67bb829043514a1f285a5c
Mike Graves <mgraves@mit.edu>
2018-03-15 11:15:06 -0400

    Add OSM HOT base map
    Switch to using the OpenStreetMap humanitarian data base layer.

---
+++ app/assets/javascripts/geoweb.js
@@ -0,0 +1,11 @@
+/**
+ * Custom GeoWeb modifications. This is included last.
+ *
+**/
+
+GeoBlacklight.Basemaps['osmhot'] = L.tileLayer(
+  'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
+  }
+);
