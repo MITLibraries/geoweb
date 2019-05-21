44fc3e59125b83113bd8fdee6e88e661fecdf931
Jeremy Prevost <jprevost@mit.edu>
2019-05-21 12:03:13 -0400

    Adds support for developer auth and saml auth
    Developer auth is super helpful for local development, PR builds and possibly staging dependent on how much auth flows matter for the app (i.e. do you need to test this in a prod-like environment before prod... if so, use the same auth as prod).
    
    SAML auth is likely a good option for production.
    
    Shibboleth / mod_shib support is retained for legacy reasons.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,17 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_action :verify_authenticity_token
+
   def shibboleth
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    sign_in_and_redirect @user, event: :authentication
+  end
+
+  def saml
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    sign_in_and_redirect @user, event: :authentication
+  end
+
+  def developer
     @user = User.from_omniauth(request.env['omniauth.auth'])
     sign_in_and_redirect @user, event: :authentication
   end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,7 +2,48 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
 
-  config.omniauth :shibboleth, { info_fields: { email: 'mail' } }
+  if ENV['AUTH_TYPE'] == 'developer'
+    config.omniauth :developer
+  elsif ENV['AUTH_TYPE'] == 'saml'
+    # Omniauth Config
+    idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
+
+    # Load IdP metadata from a file in test ENV
+    if Rails.env.test?
+      idp_data = File.open("#{Rails.root}/test/fixtures/files/testshib-providers.xml")
+      idp_metadata = idp_metadata_parser.parse_to_hash(idp_data)
+    # Load IdP metadata directly from the IdP in dev / prod ENV
+    else
+      idp_metadata = idp_metadata_parser.parse_remote_to_hash(
+        ENV['IDP_METADATA_URL'],
+        true, # validate cert
+        entity_id: ENV['IDP_ENTITY_ID']
+      )
+    end
+
+    config.omniauth :saml,
+                    idp_cert_fingerprint: idp_metadata[:idp_cert_fingerprint],
+                    idp_sso_target_url: ENV['IDP_SSO_URL'],
+                    idp_cert: idp_metadata[:idp_cert],
+                    certificate: Base64.strict_decode64(ENV['SP_CERTIFICATE']),
+                    private_key: Base64.strict_decode64(ENV['SP_PRIVATE_KEY']),
+                    issuer: ENV['SP_ENTITY_ID'],
+                    request_attributes: {},
+                    attribute_statements: { uid: [ENV['URN_UID']],
+                                            email: [ENV['URN_EMAIL']],
+                                            given_name: [ENV['URN_GIVEN_NAME']],
+                                            surname: [ENV['URN_SURNAME']] },
+                    security: { authn_requests_signed: true,
+                                want_assertions_signed: true,
+                                want_assertions_encrypted: true,
+                                metadata_signed: true,
+                                embed_sign: false,
+                                digest_method: XMLSecurity::Document::SHA1,
+                                signature_method: XMLSecurity::Document::RSA_SHA1 }
+
+  else
+    config.omniauth :shibboleth, { info_fields: { email: 'mail' } }
+  end
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
