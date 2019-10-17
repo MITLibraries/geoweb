95cb29fc7658eaf8fb61fa0971b6a3f5c6606975
Jeremy Prevost <jprevost@mit.edu>
2019-10-17 14:49:38 -0400

    Allows configuration of throttle via env and logs throttles
    THROTTLE_TIME and THROTTLE_COUNT are now available. The default values will result in an approximately 30 requests per minute throttle per IP.

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -3,11 +3,29 @@ class Rack::Attack
   # rack attack with just use the rails cache
   Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
 
+  THROTTLE_TIME = ENV.fetch("THROTTLE_TIME", 5).to_i
+  THROTTLE_COUNT = ENV.fetch("THROTTLE_COUNT", 150).to_i
+
   # Throttle non asset and non ogc requests by IP (30rpm)
   # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
-  throttle('req/ip', limit: 150, period: 5.minutes) do |req|
+  throttle('req/ip', limit: THROTTLE_COUNT, period: THROTTLE_TIME.minutes) do |req|
     unless req.path.start_with?('/ogc') || req.path.start_with?('/assets')
       req.ip
     end
+  end
+
+  Rack::Attack.throttled_response = lambda do |env|
+    [ 429, {}, ["The site is experiencing heavy usage now. Please try your request again in a few minutes.\n"]]
+  end
+
+  # Log when throttles are triggered
+  ActiveSupport::Notifications.subscribe("throttle.rack_attack") do |name, start, finish, request_id, payload|
+    @@rack_logger ||= ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+    @@rack_logger.info{[
+      "[#{payload[:request].env['rack.attack.match_type']}]",
+      "[#{payload[:request].env['rack.attack.matched']}]",
+      "[#{payload[:request].env['rack.attack.match_discriminator']}]",
+      "[#{payload[:request].env['rack.attack.throttle_data']}]",
+      ].join(' ') }
   end
 end
