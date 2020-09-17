f33e49f0d383619fa2d2841ea89a0bc9485cd5c7
Adam Jazairi <jazairi@mit.edu>
2020-09-17 13:54:28 -0400

    Use GBL public? method for checking rights info instead of custom public_file? method

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -5,7 +5,7 @@ class DownloadController < ApplicationController
   def file
     @response, @document = search_service.fetch(params[:id])
 
-    if public_file? || current_user
+    if @document.public? || current_user
       redirect_to presign_this(s3_key_guesser)
     else
       flash[:error] = 'Authentication required'
@@ -14,22 +14,7 @@ class DownloadController < ApplicationController
   end
 
   private
-
-  # Checks whether a document is public, if not require user to authenticate
-  def restricted_should_authenticate
-    authenticate_user! unless @document.public?
-  end
-
-  # The Blacklight way of doing this is to leverage a Presenter.
-  # It was unclear whether that could work in a Controller cleanly.
-  # It is possible there are other ways GeoBlacklight is determining
-  # the ability to download files other than this single field check.
-  # Note: We aren't protecting against invalid @document because that
-  # seems to be done as part of blacklight.
-  def public_file?
-    @document[1]._source['dc_rights_s'] == 'Public'
-  end
-
+  
   # All of our files are stored in S3 as zip files. This reconstructs
   # the file name based on how the layer_id_s is being set outside of
   # this app during the data loading process.
