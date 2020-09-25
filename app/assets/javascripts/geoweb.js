67b7082476b23396b91351b85fc784f53febcb52
Adam Jazairi <jazairi@mit.edu>
2020-09-25 12:39:39 -0400

    Remove custom osmhot basemap, now that OSM is included in GBL

--- app/assets/javascripts/geoweb.js
+++ app/assets/javascripts/geoweb.js
@@ -3,13 +3,6 @@
  *
 **/
 
-GeoBlacklight.Basemaps['osmhot'] = L.tileLayer(
-  'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
-  }
-);
-
 GeoBlacklight.Viewer.Map.mergeOptions({
   bbox: [[-45, -154], [65, 145]]
 });
